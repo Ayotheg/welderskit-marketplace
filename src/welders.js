ac6c49f9d94818b500f6bbc09f83a8a0addbb232
Omoniyi Ayoola <haywhyshotz@gmail.com>
2025-10-03 04:40:44 -0700

    Update welders.js

--- src/welders.js
+++ src/welders.js
@@ -1,22 +1,25 @@
+import pfp from "./assets/pfp.png"
+
+
 export const welders = [
   {
     name: "Mr Awo",
     bio: "Seasoned welder, 5k likes and certified",
-    image: "./src/assets/pfp.png",
+    image: pfp,
     phone: "+2348101734037",
     award: "✅ Verified Welder"
   },
   {
     name: "Mrs Ade",
     bio: "Expert stainless steel welder, 3k likes",
-    image: "./src/assets/pfp.png",
+    image: pfp,
      phone: "+2348101734037",
      award: "🏅 Top Rated"
   },
   {
     name: "John Doe",
     bio: "Aluminum welding specialist",
-    image: "./src/assets/pfp.png",
+    image: pfp,
      phone: "+2348101734037",
       award: "✅ Verified Welder"
   },
