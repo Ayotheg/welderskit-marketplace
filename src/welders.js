8c69434a8e47753d63025479f83b3489bd4b199e
Omoniyi Ayoola <haywhyshotz@gmail.com>
2025-10-08 10:24:52 -0700

    Update welders.js

--- src/welders.js
+++ src/welders.js
@@ -9,21 +9,24 @@ export const welders = [
     bio: "Seasoned welder, 5k likes and certified. I approach every work with precision and carefulness.",
     image: john,
     phone: "+2348101734037",
-    award: "✅ Verified Welder"
+    award: "✅ Verified Welder",
+    report:"www.google.com"
   },
   {
     name: "Mr Soliu",
     bio: "Expert stainless steel welder, I deliver quality works for my customers",
     image: awo,
      phone: "+2348033227792",
-     award: "🏅 Top Rated"
+     award: "🏅 Top Rated",
+     report:"report"
   },
   {
     name: "mr tejuosho",
     bio: "Aluminum welding specialist. Send me a dm or call let's get to business",
     image: ayo,
      phone: "+2348050399188",
-      award: "✅ Verified Welder"
+      award: "✅ Verified Welder",
+      report:"report"
   },
   
 ]
